71a31e6e189310da80bfcd450e06ef49836e771a
2002mk <ganesanmanisha16@gmail.com>
2024-02-01 11:57:11 +0530

    creat new folders for lab doc

---
+++ Program/rsa.py
@@ -0,0 +1,15 @@
+import random
+class rsa:
+    def __init__(self,pub,prv) -> None:
+        self.pub=pub
+        self.prv=prv
+    
+    def publicKey(self):
+    #def encrypt(self,pub,prv):
+
+
+
+class rsa_Encrypt:
+    def __init__(self,pub,prv) -> None:
+        self.pub=pub
+        self.prv=prv
