10d7988cbf2cb2eb0cb284e315f5fef6f43c7098
2002mk <ganesanmanisha16@gmail.com>
2024-05-25 08:35:11 +0530

    create input for banker's alg ang quick short

--- try program/bankers_alg.py
+++ try program/bankers_alg.py
@@ -1,10 +1,24 @@
-def proc_in(p):
+def proc_in(p,r):
     pr=list()
     for i in range(p):
-        pr.append(list(map(int,input(f"Enter the processor{i}:").split())))
+            aloc=list(map(int, input(f"Enter the aloccation processor{i} for {r}with space between:").split()))
+            pr.append(aloc)
     return pr
+
+def max_aloc(p,r):
+    m_pr=list()
+    for i in range(p):
+            aloc=list(map(int, input(f"Enter the aloccation processor{i} for {r}with space between:").split()))
+            m_pr.append(aloc)
+    return m_pr
 
 
 p=int(input("Enter the number of processor:"))
 r=int(input("Enter the number of resorces:"))
-
+pr=proc_in(p,r)
+print(pr)
+print("Maximum alocation matrix:")
+MaxAlo=max_aloc(p,r)
+print(MaxAlo)
+av_re=list(map(int, input("Enter the available resource:").split()))
+print(av_re)
--- try program/test.py
+++ try program/test.py
@@ -1,33 +1,25 @@
-def str_dec(msg):
-    int_m=[]
-    print("string to decimal")
-    for i in msg:
-        int_m.append(ord(i))
-    print(int_m)
-    return int_m
+import random
+def RQS(arr):
+    n=len(arr)
+    if n<=1:
+        return arr
+    pivot=random.choice(arr)
+    left=[]
+    right=[]
+    for i in range(0,n):
+        if(arr[i] > pivot):
+            left.append(i)
+        else:
+            right.append(i)
+    return RQS(left) +[pivot]+ RQS(right)
 
-def enc(msg,e,n):
-    en=str_dec(msg)
-    new_msg=[]
-    for i in en:
-        new_msg.append((i**e)%n)
-    # en=[chr(i) for i in en]
-    return new_msg
-def dec(new_msg,d,n):
-    omsg=[]
-    for i in new_msg:
-        omsg.append((i**d)%n)
-    print("new string list is:",omsg)
-    return ''.join(chr(i) for i in omsg)
-        
-e=17
-d=2753
-msg="hello"
-print(msg)
-n=3233
-new_msg=[]
-new_msg=enc(msg,e,n)
-print("encripted Message",new_msg)
-or_msg=[]
-or_msg=dec(new_msg,d,n)
-print(or_msg)
+
+
+n=int(input("Enter the number of elements in array:"))
+arr=[]
+for i in range(0,n):
+    a=int(input("Enter the element or array:"))
+    arr.append(a)
+print(arr)
+r=RQS(arr)
+print(r)
